a2878cda36760b6cb95451005ce82bec3d1b1f33
Fabitti <fabianoliva01@hotmail.com>
2023-08-21 18:52:04 -0300

    Nav_Bar base styles

---
+++ src/components/Nav_bar/index.jsx
@@ -0,0 +1,29 @@
+import "./index.css"
+import logo from "../../../public/shared/logo.svg"
+
+const Nav_Bar = () => {
+    return ( <div className="Nav_Bar_Cont">
+    <div className="Nav_Bar_Logo">  
+    <img src={logo} alt="" />
+    <div className="Nav_Bar_Logo_Line"></div>
+    </div>
+    <div className="Nav_Bar_Index">
+        <ul className="Nav_Bar_Index_ul">
+            <li className="Nav_Bar_Index_El_1">
+                <a href="#"><span className="Nav_Bar_Index_El_Number">00</span><span className="Nav_Bar_Index_El_Name">Home</span></a>
+                </li>
+            <li className="Nav_Bar_Index_El_2">
+                <a href="#"><span className="Nav_Bar_Index_El_Number">01</span><span className="Nav_Bar_Index_El_Name">Destination</span></a>
+                </li>
+            <li className="Nav_Bar_Index_El_3">
+                <a href="#"><span className="Nav_Bar_Index_El_Number">02</span ><span className="Nav_Bar_Index_El_Name">Crew</span></a>
+                </li>
+            <li className="Nav_Bar_Index_El_4"> 
+            <a href="#"><span className="Nav_Bar_Index_El_Number">03</span><span className="Nav_Bar_Index_El_Name">Technology</span></a>
+            </li>
+        </ul>
+    </div>
+    </div> );
+}
+ 
+export default Nav_Bar;
